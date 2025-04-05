7362103da14453211bffc246a604cbf4ca77c93d
Hemed Awadh <ASHemed@kcbgroup.com>
2025-04-05 12:30:10 +0300

    updated tests

--- src/test/java/com/example/studentManagementApp/web/controller/StudentControllerTest.java
+++ src/test/java/com/example/studentManagementApp/web/controller/StudentControllerTest.java
@@ -1,13 +1,13 @@
 package com.example.studentManagementApp.web.controller;
 
+import com.example.studentManagementApp.domain.Student;
 import com.example.studentManagementApp.service.StudentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -20,6 +20,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.ArrayList;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 /**
  * Project: studentManagementApp
@@ -35,7 +36,7 @@ import static org.mockito.Mockito.when;
 class StudentControllerTest {
     @Autowired
     private StudentController studentController;
-    @MockBean
+    @MockitoBean
     private StudentService studentService;
 
     @Test
@@ -52,26 +53,65 @@ class StudentControllerTest {
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
 
 
+    }
+
+    @Test
+    @DisplayName("Test get Students By id")
+    void getStudentById() throws Exception {
+        //given
+        Student student = new Student();
+        student.setId(1L);
+        student.setFirstName("John");
+        when(studentService.getStudentById(1L)).thenReturn(student);
+        MockHttpServletRequestBuilder getRequest = MockMvcRequestBuilders.get("/api/v1/students/1");
+        //When && assert
+        MockMvcBuilders.standaloneSetup(studentController)
+                .build()
+                .perform(getRequest)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.firstName").value("John"));
+
 
     }
 
-//    @Test
-//    void getStudentById() {
-//    }
-//
-//    @Test
-//    void updateStudent() {
-//    }
-//
-//    @Test
-//    void createStudent() {
-//    }
-//
-//    @Test
-//    void getStudentByEmail() {
-//    }
-//
-//    @Test
-//    void getStudentByPartialId() {
-//    }
+    @Test
+    @DisplayName("Test update Students")
+    void updateStudent() throws Exception {
+        //given
+        Student student = new Student();
+        student.setId(1L);
+        student.setFirstName("John");
+
+        when(studentService.updateStudent(student, student.getId())).thenReturn(student);
+        String studentJson = new ObjectMapper().writeValueAsString(student);
+
+        MockHttpServletRequestBuilder getRequest = MockMvcRequestBuilders
+                .put("/api/v1/students/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(studentJson);
+        //When && assert
+        MockMvcBuilders.standaloneSetup(studentController)
+                .build()
+                .perform(getRequest)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.firstName").value("John"));
+
+
+    }
+
+    @Test
+    void createStudent() {
+    }
+
+    @Test
+    void getStudentByEmail() {
+    }
+
+    @Test
+    void getStudentByPartialId() {
+    }
 }
